b7b1c09cc7c6f49987b886ef9c3079cdbff2efa6
Viktor Kronvall <viktor.kronvall@gmail.com>
2014-08-20 11:27:43 +0200

    Chronological order.

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ module.exports = function(year) {
     return [
         new Date(year, 0, 1),                   //nyårsdagen
         new Date(year, 0, 6),                   //trettonddag jul
+        easterOffset(-2),                       //Långfredag
+        easterOffset(1),                        //Annandag påsk
         new Date(year, 4, 1),                   //1 maj
+        easterOffset(39),                       //Kristi himmelsfärds dag
         new Date(year, 5, 6),                   //Nationaldagen
+        nextWeekday(new Date(year, 5, 20), 6),  //Midsommardagen
+        nextWeekday(new Date(year, 9, 31), 6),  //Alla helgons dag
         new Date(year, 11, 24),                 //Julafton
         new Date(year, 11, 25),                 //Juldagen
         new Date(year, 11, 26),                 //Annandag Jul
         new Date(year, 11, 31),                 //Nyårsafton
-        nextWeekday(new Date(year, 5, 20), 6),  //Midsommardagen
-        nextWeekday(new Date(year, 9, 31), 6),  //Alla helgons dag
-        easterOffset(-2),                       //Långfredag
-        easterOffset(1),                        //Annandag påsk
-        easterOffset(39),                       //Kristi himmelsfärds dag
     ];
 };
