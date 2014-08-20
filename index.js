ee2c67849d78a7a1765d1f7013dfa0c06559655e
Viktor Kronvall <viktor.kronvall@gmail.com>
2014-08-20 11:21:20 +0200

    Move to Swedish holidays.

--- index.js
+++ index.js
@@ -6,24 +6,21 @@ var nextWeekday = utils.nextWeekday;
 
 module.exports = function(year) {
     var easter = easterDay(year);
-    var easterOffset = function(days) { return offsetDays(easter, days); }
+    var easterOffset = function(days) { return offsetDays(easter, days); };
 
     return [
-        new Date(year, 0, 1),       // 1. janúar
-        easterOffset(-3),           // Skírdagur
-        easterOffset(-2),           // Föstudagurinn langi
-        easter,                     // Páskadagur
-        easterOffset(1),            // Annar i páskum
-        nextWeekday(new Date(year, 3, 19), 4), // Sumardagurinn fyrsti
-        new Date(year, 4 , 1),      // 1. maí
-        easterOffset(39),           // Uppstigningardagur
-        easterOffset(49),           // Hvítasunnudagur
-        easterOffset(50),           // Annar í hvítasunnu
-        new Date(year, 5, 17),      // 17. júni
-        nextWeekday(new Date(year, 7, 1), 1), // Frídagur verslunarmanna
-        new Date(year, 11, 24, 13), // Aðfangadagur
-        new Date(year, 11, 25),     // Jóladagur
-        new Date(year, 11, 26),     // Annar í jólum
-        new Date(year, 11, 31, 13)  // Gamlársdagur
+        new Date(year, 0, 1),                   //nyårsdagen
+        new Date(year, 0, 6),                   //trettonddag jul
+        new Date(year, 4, 1),                   //1 maj
+        new Date(year, 5, 6),                   //Nationaldagen
+        new Date(year, 11, 24),                 //Julafton
+        new Date(year, 11, 25),                 //Juldagen
+        new Date(year, 11, 26),                 //Annandag Jul
+        new Date(year, 11, 31),                 //Nyårsafton
+        nextWeekday(new Date(year, 5, 20), 6),  //Midsommardagen
+        nextWeekday(new Date(year, 9, 31), 6),  //Alla helgons dag
+        easterOffset(-2),                       //Långfredag
+        easterOffset(1),                        //Annandag påsk
+        easterOffset(39),                       //Kristi himmelsfärds dag
     ];
-}
+};
